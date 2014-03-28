db1a7c7a8d3d617b0546dcf71873f8c397437d41
Sander van Harmelen <svanharmelen@schubergphilis.com>
2014-03-28 17:52:23 +0100

    Initial commit...

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+sbp_netmon CHANGELOG
+====================
+
+
+0.1.0
+-----
+- Sander van Harmelen / Ane van Straten - Initial release of netmon
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'sbp_netmon'
+maintainer       'Sander van Harmelen'
+maintainer_email 'svanharmelen@schubergphilis.com'
+license          'Apache 2.0'
+description      'Installs/Configures netmon'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'windows'
